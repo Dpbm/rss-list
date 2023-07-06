8ce5559c0e56e4badaa859e159a697b87fd876a7
Dpbm <dpbm136@gmail.com>
2023-07-06 10:36:35 -0300

    fixed arguments

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,71 @@ use std::path;
 use std::fs;
 use rss::Channel;
 use clap::Parser;
+use rss::Item;
 use std::error::Error;
 
 #[derive(Parser)]
 struct Cli {
-    feed: path::PathBuf,
+    #[arg(short='u', long="url")]
+    url: Option<String>,
+    #[arg(short='f', long="feed")]
+    feed: Option<path::PathBuf>,
 }
 
-
 #[tokio::main]
-async fn main() {
+async fn main(){
     let args = Cli::parse();
     
-    let feed: path::PathBuf = args.feed;
+    match &args.url{
+        Some(url) => get_one(url.to_string()).await,
+        None => {}
+    }
+    
+    match &args.feed{
+        Some(feed) => get_multiple(feed.to_path_buf()).await,
+        None => {}
+    }
+}    
 
-    let feed_content = fs::read_to_string(&feed).expect("Feed File not found!!!");
+    
+
+async fn get_one(url:String){
+    let result = get_rss(url).await;
+
+    match result{
+        Ok(channel) => {
+            println!("{}",&channel.title);
+
+            for item in &channel.items{
+                show_item(&item);
+            }
+        },
+        Err(_) => println!("Error")
+    }
+
+}
+
+async fn get_multiple(feed: path::PathBuf){
+    let feed_content = fs::read_to_string(feed).expect("Feed File not found!!!");
     let feed_list = feed_content.lines();
     
     for url in feed_list{
-        let result = get_rss(url.to_string()).await;
-        match result{
-            Ok(channel) => {
-                println!("{}",&channel.title);
-                for item in &channel.items{
-                    match &item.title{
-                        Some(item_title) => println!("-> {}", &item_title),
-                        None => println!("-> NULL")
-                    } 
-                }
-            }
-            Err(_) => println!("Error")
-        }
+        get_one(url.to_string()).await;
     }
+}
+
+fn show_item(item:&Item){
+    match &item.title{
+        Some(title) => println!("----{}----", title),
+        None => println!("NULL")
+    }
+
+    match &item.link{
+        Some(url) => println!("{}", url),
+        None => println!("NO LINK!")
+    }
+
+    println!("");
 }
 
 async fn get_rss(url:String) -> Result<Channel, Box<dyn Error>> {
@@ -42,6 +75,8 @@ async fn get_rss(url:String) -> Result<Channel, Box<dyn Error>> {
         .await?
         .bytes()
         .await?;
+
     let channel = Channel::read_from(&content[..])?; 
+
     Ok(channel)
 }
